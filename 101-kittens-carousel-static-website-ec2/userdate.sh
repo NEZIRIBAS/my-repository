99f070b530b4b916f9a460d8506ad78ca4978f4c
NEZIRIBAS <neziribas@outlook.com>
2025-02-08 18:49:27 -0500

    update index.html

--- 101-kittens-carousel-static-website-ec2/userdate.sh
+++ 101-kittens-carousel-static-website-ec2/userdate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
 
 yum update -y
+yum install httpd -y
 
+cd /var/www/html
 
 FOLDER="https://raw.githubusercontent.com/NEZIRIBAS/my-repository/refs/heads/main/101-kittens-carousel-static-website-ec2/static-web"
 
